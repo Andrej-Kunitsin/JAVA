3cc3239abcdaff272105a281e946ab555f1ecdef
Jack_killer <Jack_killer@mail.ru>
2015-02-03 20:30:27 +0200

    pre5

--- LlistSOfTestNG/src/testNG/ExListTest.java
+++ LlistSOfTestNG/src/testNG/ExListTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class ExListTestTest extends Assert {
+public class ExListTest extends Assert {
 	ExList xx;
 
 	@BeforeMethod
