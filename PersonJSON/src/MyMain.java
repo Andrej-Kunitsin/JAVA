17d5e46016c878cd6ac89d35e4edbbc13d06cc3b
jack_killer <jack_killer@Jack-NoteBook>
2015-02-14 22:26:23 +0200

    qq

--- PersonJSON/src/MyMain.java
+++ PersonJSON/src/MyMain.java
@@ -1,14 +1,21 @@
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
+import javax.lang.model.util.Elements;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-public class MyMain {
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
 
-	public static void main(String[] args) throws Exception  {
+public class MyMain
+{
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException
+	{
 		List<Person> list = new ArrayList<Person>();
 		list.add(new Person(1, "Dash1", "Solo1", 10));
 		list.add(new Person(2, "Dash2", "Solo2", 20));
@@ -19,8 +26,13 @@ public class MyMain {
 		list.add(new Person(7, "Dash7", "Solo7", 70));
 		list.add(new Person(8, "Dash8", "Solo8", 80));
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		DocumentBuilder db = dbf.newDocumentBuilder();
-		
+		DocumentBuilder db = null;
+		Document doc = null;
+			db = dbf.newDocumentBuilder();
+			doc = db.parse("/media/jack_killer/work/qqq.xml");
+		Element q =(Element) doc.getDocumentElement();
+		Elements Q=  (Elements) q.getAttributes();
+		System.out.println(Q);
 	}
 
 }
